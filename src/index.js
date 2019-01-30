db85456135a8f37814a896b9be8cf1da56a74a11
sosisvesalam <colakilgaz@gmail.com>
2019-01-30 21:47:18 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'tachyons';
+import App from './containers/App';
+import './index.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
